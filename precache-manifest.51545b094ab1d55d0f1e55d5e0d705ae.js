a8d0cf773ccf3702499eff9e87116d409cb080ac
Litty Ling <porkythemorky@gmail.com>
2020-01-05 12:40:56 -0500

    Updates

---
+++ precache-manifest.51545b094ab1d55d0f1e55d5e0d705ae.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3290d20a02f831f4b3a51bbcabeec63b",
+    "url": "/jhn/index.html"
+  },
+  {
+    "revision": "8f099c308cd5e439c546",
+    "url": "/jhn/static/css/main.b2666f4f.chunk.css"
+  },
+  {
+    "revision": "1b2aa6311ee818cedf08",
+    "url": "/jhn/static/js/2.27368161.chunk.js"
+  },
+  {
+    "revision": "783efe56c297789f679b639c509e9a30",
+    "url": "/jhn/static/js/2.27368161.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f099c308cd5e439c546",
+    "url": "/jhn/static/js/main.3a807323.chunk.js"
+  },
+  {
+    "revision": "56075d94902bf1fc7041",
+    "url": "/jhn/static/js/runtime-main.788d3e50.js"
+  },
+  {
+    "revision": "cbc9cd476422e89523640c4607b0b68a",
+    "url": "/jhn/static/media/asteroid.cbc9cd47.png"
+  },
+  {
+    "revision": "8dfd5ff781ae50db807c64d5d2025703",
+    "url": "/jhn/static/media/baby.8dfd5ff7.png"
+  },
+  {
+    "revision": "0088e89581fe6b77faf59aef5b6841a6",
+    "url": "/jhn/static/media/babyearth.0088e895.png"
+  },
+  {
+    "revision": "4a592eaa7f851ac7b8f3fc783dd550fc",
+    "url": "/jhn/static/media/book.4a592eaa.png"
+  },
+  {
+    "revision": "9f7f49270c1b87928cc4429c42fb6d5a",
+    "url": "/jhn/static/media/catfoodwork.9f7f4927.jpg"
+  },
+  {
+    "revision": "d58d3312a34038b1f5588f60d6a43933",
+    "url": "/jhn/static/media/comet.d58d3312.png"
+  },
+  {
+    "revision": "fc7e9be4142b84297c5d15bb08c41742",
+    "url": "/jhn/static/media/communism.fc7e9be4.jpg"
+  },
+  {
+    "revision": "7ffc7acad7150ef9392128dc792fc4e8",
+    "url": "/jhn/static/media/crocs.7ffc7aca.jpg"
+  },
+  {
+    "revision": "9fdbc1482b4b0eddc2c22b631e60d717",
+    "url": "/jhn/static/media/crocswork.9fdbc148.jpg"
+  },
+  {
+    "revision": "38b3f87a4a80c7ba4320cf8ca34dd038",
+    "url": "/jhn/static/media/devourer.38b3f87a.JPG"
+  },
+  {
+    "revision": "0ce973b38f67a1cde5519e42d6572e99",
+    "url": "/jhn/static/media/dewalt.0ce973b3.jpg"
+  },
+  {
+    "revision": "6c0826208579be65b03c2d763f2c3b5e",
+    "url": "/jhn/static/media/dewaltwork.6c082620.jpg"
+  },
+  {
+    "revision": "1ba45aa460d26bf5de327bbac991234d",
+    "url": "/jhn/static/media/dinosaurs.1ba45aa4.jpg"
+  },
+  {
+    "revision": "0c30b5cead18e96f790ff933cbbe023c",
+    "url": "/jhn/static/media/ducks.0c30b5ce.jpg"
+  },
+  {
+    "revision": "261b28dd508b98f9b663a0f45a1f8372",
+    "url": "/jhn/static/media/egypt.261b28dd.jpg"
+  },
+  {
+    "revision": "317c46c8c0c13c441edfa53404e3c23e",
+    "url": "/jhn/static/media/friends.317c46c8.jpg"
+  },
+  {
+    "revision": "1bb2b378ed2e5047cdab1a0d15f6fba2",
+    "url": "/jhn/static/media/frozen.1bb2b378.jpg"
+  },
+  {
+    "revision": "7e7a94349e09b13aa23add3ee9b179eb",
+    "url": "/jhn/static/media/galaxy.7e7a9434.png"
+  },
+  {
+    "revision": "c371a54b2f386b485c071cc985133c1e",
+    "url": "/jhn/static/media/hospital.c371a54b.jpg"
+  },
+  {
+    "revision": "801786fa5295257912c8d9667b7d7de1",
+    "url": "/jhn/static/media/hu.801786fa.jpg"
+  },
+  {
+    "revision": "b62740699684c4a0cfb2ea6959e4040b",
+    "url": "/jhn/static/media/hug.b6274069.JPG"
+  },
+  {
+    "revision": "3c759aa59d0a5f96464a34c3c6de1852",
+    "url": "/jhn/static/media/jhn.3c759aa5.mp4"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/jhn/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "c918060c7e17d5f91c816bf6378eaef5",
+    "url": "/jhn/static/media/mao.c918060c.jpg"
+  },
+  {
+    "revision": "50d09f42115edfd128f826cabb9c851f",
+    "url": "/jhn/static/media/mariachi.50d09f42.jpg"
+  },
+  {
+    "revision": "0396120d70958939016434f1564ac15e",
+    "url": "/jhn/static/media/robotwork.0396120d.gif"
+  },
+  {
+    "revision": "f0d7e560c5654e3f3827fb21b7997b21",
+    "url": "/jhn/static/media/scholar.f0d7e560.JPG"
+  },
+  {
+    "revision": "ff457d12a8bee7ab09654f2966ee1bc2",
+    "url": "/jhn/static/media/space.ff457d12.jpg"
+  },
+  {
+    "revision": "09377f68e26b77cba97f33021150969b",
+    "url": "/jhn/static/media/spacerotated.09377f68.jpg"
+  },
+  {
+    "revision": "653e7c77ebc808ecf91fcda16ef718bf",
+    "url": "/jhn/static/media/spongebob.653e7c77.jpg"
+  },
+  {
+    "revision": "9eb9ccc2baa9f4945b5d739ff3e55307",
+    "url": "/jhn/static/media/spongebobmovie.9eb9ccc2.jpg"
+  },
+  {
+    "revision": "045581a760f218928c313a2184cad699",
+    "url": "/jhn/static/media/tyson.045581a7.jpg"
+  },
+  {
+    "revision": "7e9c95d75e74921ba323f0d274f75479",
+    "url": "/jhn/static/media/utilitynailswork.7e9c95d7.jpg"
+  },
+  {
+    "revision": "f1b61608f35e3beebf588566712910b2",
+    "url": "/jhn/static/media/water.f1b61608.jpg"
+  },
+  {
+    "revision": "e552b37c97e35cea6997552693ad256d",
+    "url": "/jhn/static/media/wateringcan.e552b37c.jpg"
+  },
+  {
+    "revision": "9a6845c2eb6a8458b44603b34e1a36e5",
+    "url": "/jhn/static/media/wateringcanwork.9a6845c2.jpg"
+  },
+  {
+    "revision": "3382ad0e7ea2c9018f19993e1a67df93",
+    "url": "/jhn/static/media/wheelwork.3382ad0e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mywebsite/precache-manifest.5ffdc466fb5c6959877ea6765490408a.js"
+  "/jhn/precache-manifest.51545b094ab1d55d0f1e55d5e0d705ae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mywebsite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jhn/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
